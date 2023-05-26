2a346a2e08b95cb901c36b044b847ae86a316938
Nick Brook <nrbrook@gmail.com>
2023-05-26 16:42:42 +0100

    When retransmitting frames, MIN should check for tx space

--- target/min.c
+++ target/min.c
@@ -385,9 +385,15 @@ static void valid_frame_received(struct min_context *self)
                 // Now retransmit the number of frames that were requested
                 for(i = 0; i < num_nacked; i++) {
                     struct transport_frame *retransmit_frame = &self->transport_fifo.frames[idx];
-                    transport_fifo_send(self, retransmit_frame);
-                    idx++;
-                    idx &= TRANSPORT_FIFO_SIZE_FRAMES_MASK;
+                    if(ON_WIRE_SIZE(retransmit_frame->payload_len) <= min_tx_space(self->port)) {
+                        transport_fifo_send(self, retransmit_frame);
+                        idx++;
+                        idx &= TRANSPORT_FIFO_SIZE_FRAMES_MASK;
+                    }
+                    else {
+                        min_debug_print("Not enough space to retransmit frame\n");
+                        break;
+                    }
                 }
             }
             else {
